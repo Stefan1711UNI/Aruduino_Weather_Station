aeede120e1c771a444532fd899a27676599af863
kamy-99 <kamyab.rayganshirazinejad@student.nhlstenden.com>
2024-10-14 22:20:57 +0200

    LDR

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+// KAMY
+#define LDR A0 // LDR at port A0
+int ldr;
+int LED = 13; // On-board LED at 13
+int RELAY = 2; // RELAY at port 2
 
-void setup() {
+void setup() 
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ pinMode(LED, OUTPUT); // LED is output
+ pinMode(RELAY, OUTPUT); // RELAY is output
+ digitalWrite(LED, LOW); // LED OFF at beginning
+ digitalWrite(RELAY, LOW); // RELAY OFF at beginning
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void LIGHT()
+{
+  ldr = analogRead(LDR); // Read temperature
+  if(ldr > 800) // If dark detected
+  {
+  digitalWrite(LED, HIGH); // LED ON
+  digitalWrite(RELAY, HIGH); // RELAY ON
+  }
+  else
+  {
+  digitalWrite(LED, LOW); // LED OFF
+  digitalWrite(RELAY, LOW); // RELAY OFF
+  }
 }
