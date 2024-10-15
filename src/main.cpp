3c156430eaae49fc609eabe9a29279e6cb54ce23
Stefan1711UNI <stefan.sonderling1@student.nhlstenden.com>
2024-10-15 13:43:41 +0200

    Many changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
+#include <Arduino.h>
 #include "DHT.h"
 #include "LCD_I2C.h"
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <LCD_I2C.h>
+
+//functions
+int readDHT();
+void getMax(double insideTemp, double hum);
+void disDHT();
+void LIGHT();
+
+
 
 // KAMY
 #define LDR A0 // LDR at port A0
@@ -7,13 +20,9 @@ int ldr;
 int LED = 13; // On-board LED at 13
 int RELAY = 2; // RELAY at port 2
 
-//STEFAN SONDERLING
-//DHT11 SENSOR WILL NOT WORK ON VSCODE AS THE LIBARY DOES NOT EXIST. PLUG BACK INTO ARDUINO IDE ENVIROMENT
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 LCD_I2C lcd(0x27, 16, 2);
 
-#define dhtPin 2  //DHT sensor on pin 2
+#define dhtPin 8  //DHT sensor on pin 2
 DHT dht(dhtPin, DHT11);    
 
 //global variable can be accessed anyware
@@ -37,7 +46,7 @@ void setup() {
 
 void loop() {
   disDHT();
-
+  LIGHT();
 }
 
 void getMax(double insideTemp, double hum){
@@ -60,7 +69,7 @@ void disDHT(){
     lcd.print("Temp=");
     lcd.print(insideTemp);
     lcd.setCursor(0,1);
-    lcd.print("Humidity=");
+    lcd.print("Hum=");
     lcd.print(hum);
     lcd.print("%");
     delay(5000);
@@ -78,7 +87,7 @@ int readDHT(){
   if(isnan(hum) || isnan(insideTemp)){  //isnan checks if is not a number(error code for dht is not a number)
     return 0;
   }else {
-    maxTemp(insideTemp, hum);
+    getMax(insideTemp, hum);
     return 1;
   }
 }
@@ -90,10 +99,20 @@ void LIGHT()
   {
   digitalWrite(LED, HIGH); // LED ON
   digitalWrite(RELAY, HIGH); // RELAY ON
+  lcd.setCursor(12,0);
+  lcd.print("Night");
   }
   else
   {
   digitalWrite(LED, LOW); // LED OFF
   digitalWrite(RELAY, LOW); // RELAY OFF
+  lcd.setCursor(12,0);
+  lcd.print("Day");
   }
+}
+
+void display(){
+  disDHT();
+  LIGHT();
+  
 }
